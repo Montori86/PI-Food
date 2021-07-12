45eeee2bb4171f73132f50b2992e24dbc5fa7c9d
Montori86 <gerlm86@gmail.com>
2021-07-12 17:57:54 -0300

    config heroku

--- api/src/db.js
+++ api/src/db.js
@@ -8,29 +8,31 @@ const {
 
 } = process.env;
 
-const sequelize = process.env.MODE_ENV === "production"
+const  sequelize =
+process.env.NODE_ENV === "production"
   ? new Sequelize({
-    database: DB_NAME, 
-    dialect: "postgres",
-    host: DB_HOST, 
-    port: 5432, 
-    username: DB_HOST,
-    password: DB_PASSWORD,
-    poll: {
-      max: 3,
-      min: 1,
-      idle: 10000,
-    },
-    dialectOptions: {
-      ssl: {
-        reqire: true, 
-        rejectUnauthorized: false,    
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
       },
-      keepAlive: true,
-    },
-    ssl: true,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
     
-  }) 
   : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
